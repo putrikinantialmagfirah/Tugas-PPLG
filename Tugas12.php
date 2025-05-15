3c1437ebb32cd3cb3026bd7def72a62789325b30
putrikinantialmagfirah <putrialmagfirah01@gmail.com>
2025-05-15 14:31:58 +0800

    Tugas PPLG

---
+++ Tugas12.php
@@ -0,0 +1,22 @@
+<?php
+
+$Nama_Anggota = [
+    "Akifah Zahrani Arafat",
+    "Alqumaerah",
+    "Althaf Azza Zahra",
+    "Andi Al Firqathunnajiyah",
+    "Andi Mufrodatul Fanzila",
+    "Fahrah Furaidah Sahrir IPJ",
+    "Fauziyyah",
+    "Jilan Fathiyyah",
+    "Nadia Mirzayanti Hasbi",
+    "Rabi'atul Adawiyah",
+    "Alifah Izzah Rahmadini"
+];
+$Nama_Anggota [] = "Muftihaturrahmah";
+$Nama_Anggota [] = "Auliyah Nastity Khayla Armada";
+$Nama_Anggota [] = "Putri Kinanti Almagfirah";
+echo "Daftar Anggota Kelas 10 B </br></br>";
+foreach ($Nama_Anggota as $Daftar_Anggota_Kelas_10_B) {
+    echo "-" . $Daftar_Anggota_Kelas_10_B . "</br></br>";
+}
